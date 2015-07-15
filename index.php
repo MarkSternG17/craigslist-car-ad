11d195dc99a6ada06a89c7e13f23202f5bd51381
Mark Stern <MarkStern@gmail.com>
2015-07-15 15:23:26 -0600

    Simplified the project, eliminated specific car. Will still need an image for the project.

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!doctype html>
-	<html>
+	<html lang ="en">
 	<head>
+		<meta charset="utf-8">
 		<title> Craigslist Car Ad Data Design Project</title>
 	</head>
 	<body>
-	<p>There are several ways for a user to navigate to used & new car advertisements on the website <a href="http://albuquerque.craigslist.org">Craigslist</a><br>
-	The advertisement is for a classic 1975 Chevy mnte Carlo as pictured below.<br>
-	<img src="images/carlo.jpg" alt="Monte Carlo"></p>
+	<h3>The typical user, looking for a new or used car, will navigate to their local <a href="http://albuquerque.craigslist.org">Craigslist</a> page and follow the link for "cars+trucks" to browse the listings.</h3>
+		<h4>For this project I will focus on the buyer persona only.</h4>A buyer is typically:
+		<ul>
+			<li>Looking to buy a car.</li>
+			<li>May need a new car or may be looking for a specific car.</li>
+			<li>Generally speaking, an auto buyer on Craigslist does not need/want help financing a car.</li>
+		</ul>
 	</body>
 </html>
