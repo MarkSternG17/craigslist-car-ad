a6c5d980ea69ae2bb9df7ce38013cd32485f301a
Mark Stern <MarkStern@gmail.com>
2015-07-15 14:48:45 -0600

    Changed to 1975 Monte Carlo (more text and information about car on page

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 		<title> Craigslist Car Ad Data Design Project</title>
 	</head>
 	<body>
-	<p>This an ad from the website <a href="http://albuquerque.craigslist.org">Craigslist</a><br>
-	The advertisement is for a classic 1976 Chevy Vega as pictured below.<br>
-	<img src="images/vega.jpg" alt="Chevy Vega"></p>
+	<p>There are several ways for a user to navigate to used & new car advertisements on the website <a href="http://albuquerque.craigslist.org">Craigslist</a><br>
+	The advertisement is for a classic 1975 Chevy mnte Carlo as pictured below.<br>
+	<img src="images/carlo.jpg" alt="Monte Carlo"></p>
 	</body>
 </html>
