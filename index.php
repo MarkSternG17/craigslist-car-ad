85ca77caea10e694a929dbdb2ac89ff9b7faf78a
Mark Stern <MarkStern@gmail.com>
2015-07-15 16:26:36 -0600

    Simplified the project, eliminated specific car. Will still need an image for the project.

--- index.php
+++ index.php
@@ -11,6 +11,55 @@
 			<li>Looking to buy a car.</li>
 			<li>May need a new car or may be looking for a specific car.</li>
 			<li>Generally speaking, an auto buyer on Craigslist does not need/want help financing a car.</li>
+			<li>Most users prefer an aggregated listing of vehicles before traveling to purchase one.</li>
 		</ul>
+	</body>
+	<body>
+		<p>In this case the buyer has decided they are interested in the 1975 Chevy Monte Carlo below</p>
+		<img src="images/carlo.jpg" alt="Monte Carlo" width="150" height="112">
+	</body>
+	<body>
+		<h4>Once a buyer makes his/her choice, they are presented with several options to initiate a conversation between the buyer and seller.</h4>
+			<p>The "reply" button produces a drop-down menu that provides:</p>
+		<ul>
+			<li> Seller's name</li>
+			<li>Phone number</li>
+			<li>Email address (either secure through Craigslist or personal</li>
+		</ul>
+		<p>If the buyer, or anyone browsing the listing for that matter, notices any reason the listing should be taken down, they simply click on the "prohibited" box.
+		Craigslist will examine the listing and make the final determination. <strong>Some reasons for clicking the "prohibited" button are:</strong></p>
+		<ul>
+			<li>A buyer has discovered that the vehicle pictured is not the actual vehicle for sale.</li>
+			<li>There is offensive language/profanity within the listing.</li>
+			<li>Perhaps a savvy car buyer notices details (on the right of the page) that do not match the specific Year, Make or Model of the vehicle.</li>
+		</ul>
+	</body>
+	<body>
+		<p>Craigslist provides other useful options for buyers such as the ability to go to the previous and next listings with a simple click of a hyperlink,
+			an option to print the page and even a map shpwing the general area the vehicle is located (which is helpful if a buyer doesn't want to drive further than a certain
+		ditance).</p>
+		<p><strong>But the details matter!</strong><br>Navigating to the right side of a listing page shows you many useful details of the car you are considering:</p>
+		<dl>
+			<dt>General Information</dt>
+			<dd>Year, Make & Model</dd>
+			<dt>Condition</dt>
+			<dd>Excellent, Good, Fair</dd>
+			<dt>Drive</dt>
+			<dd>Front, Rear, 4wd, etc.,</dd>
+			<dt>Fuel</dt>
+			<dd>Gas, Biofuel, etc.,</dd>
+			<dt>Odometer Reading</dt>
+			<dd>Mileage on the car</dd>
+			<dt>Pain Color</dt>
+			<dd>Red, Blue, White, Enterprise Silver</dd>
+			<dt>Size</dt>
+			<dd>Compact, Mid, Full</dd>
+			<dt>Title Status</dt>
+			<dd>Clean title vs. No title</dd>
+			<dt>Transmission</dt>
+			<dd>Manual, Automatic, Double-Clutch, Warp Drive</dd>
+			<dt>Type</dt>
+			<dd>Coupe, Sedan, Wagon, Starship</dd>
+		</dl>
 	</body>
 </html>
