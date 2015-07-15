51d03f559a4a26ba6a61f85c321ae8ac3be4a92f
Mark Stern <MarkStern@gmail.com>
2015-07-15 06:19:58 -0600

    Starting out

---
+++ index.php
@@ -0,0 +1,11 @@
+<!doctype html>
+	<html>
+	<head>
+		<title> Craigslist ad Data Design Project</title>
+	</head>
+	<body>
+	<p>This an ad from the website <a href="albuquerque.craigslist.org">Craigslist</a><br>
+	The advertisement is for a classic 1976 Chevy Vega as pictured below.<br>
+	<img src="images/vega.jpg" alt="Chevy Vega"></p>
+	</body>
+</html>
