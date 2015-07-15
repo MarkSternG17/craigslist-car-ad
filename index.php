3887ed36cbe29376310aebb237f3ab3147d8ddfd
Mark Stern <MarkStern@gmail.com>
2015-07-15 06:42:05 -0600

    I thin I fixed the Craigslist link

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!doctype html>
 	<html>
 	<head>
-		<title> Craigslist ad Data Design Project</title>
+		<title> Craigslist Car Ad Data Design Project</title>
 	</head>
 	<body>
-	<p>This an ad from the website <a href="albuquerque.craigslist.org">Craigslist</a><br>
+	<p>This an ad from the website <a href="http://albuquerque.craigslist.org">Craigslist</a><br>
 	The advertisement is for a classic 1976 Chevy Vega as pictured below.<br>
 	<img src="images/vega.jpg" alt="Chevy Vega"></p>
 	</body>
